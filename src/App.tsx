822d6e991ccf94b3290457e811b02b75acdbee4b
mahnoor <faisalmahnoor74@gmail.com>
2024-02-21 21:06:44 +0500

    style in different file'

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import './App.css';
+import './Styles/App.css';
+import {thumbsContainer,thumb,thumbInner,img } from './Styles/AppStyle'
 
 interface CustomFile extends File {
   preview: string;
 }
-
-const thumbsContainer: React.CSSProperties = {
-  display: 'flex',
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  marginTop: 16,
-};
-
-const thumb: React.CSSProperties = {
-  display: 'inline-flex',
-  borderRadius: 2,
-  border: '1px solid #eaeaea',
-  marginBottom: 8,
-  marginRight: 8,
-  width: '100%',
-  height: '100%',
-  padding: 4,
-  boxSizing: 'border-box',
-};
-
-const thumbInner: React.CSSProperties = {
-  display: 'flex',
-  minWidth: 0,
-  overflow: 'hidden'
-};
-
-const img: React.CSSProperties = {
-  display: 'block',
-  width: 'auto',
-  height: '100%'
-};
 
 function App() {
   const [files, setFiles] = useState<CustomFile[]>([]);
